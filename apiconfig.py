62037cd3c8297a20df2495f18200583b901f4643
Startzyp <asd59541511>
2019-09-11 16:57:44 +0800

    ‘个人修改版本

--- apiconfig.py
+++ apiconfig.py
@@ -12,18 +12,18 @@ MU_SUFFIX = 'zhaoj.in'
 MU_REGEX = '%5m%id.%suffix'
 
 SERVER_PUB_ADDR = '127.0.0.1'  # mujson_mgr need this to generate ssr link
-API_INTERFACE = 'modwebapi'  # glzjinmod, modwebapi
+API_INTERFACE = 'glzjinmod'  # glzjinmod, modwebapi
 
 # Mod webapi
 WEBAPI_URL = 'https://zhaoj.in'
 WEBAPI_TOKEN = 'glzjin'
 
 # Mysql
-MYSQL_HOST = '127.0.0.1'
-MYSQL_PORT = 3306
-MYSQL_USER = 'ss'
-MYSQL_PASS = 'ss'
-MYSQL_DB = 'shadowsocks'
+MYSQL_HOST = '{HOST}'
+MYSQL_PORT = {PORT}
+MYSQL_USER = '{USER}'
+MYSQL_PASS = '{PASS}'
+MYSQL_DB = '{DB}'
 
 MYSQL_SSL_ENABLE = 0
 MYSQL_SSL_CA = ''
